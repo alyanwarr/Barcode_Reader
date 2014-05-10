c2b76fc33719fd6298b36dc20f2880ed85035962
ahmedalaa3307023 <ahmedalaa3307023@gmail.com>
2014-05-10 23:08:54 +0300

    Create Data.java

---
+++ src/Data.java
@@ -0,0 +1,47 @@
+ 
+public class Data {
+ 
+        private int generatedNumber;
+        private String packageData;
+        private String dataSent;
+        private String recievedData;
+        private int IDNumber;
+        private String choice;
+       
+        public int getGeneratedNumber() {
+                return generatedNumber;
+        }
+        public void setGeneratedNumber(int generatedNumber) {
+                this.generatedNumber = generatedNumber;
+        }
+        public String getPackageData() {
+                return packageData;
+        }
+        public void setPackageData(String packageData) {
+                this.packageData = packageData;
+        }
+        public String getDataSent() {
+                return dataSent;
+        }
+        public void setDataSent(String dataSent) {
+                this.dataSent = dataSent;
+        }
+        public String getRecievedData() {
+                return recievedData;
+        }
+        public void setRecievedData(String recievedData) {
+                this.recievedData = recievedData;
+        }
+        public int getIDNumber() {
+                return IDNumber;
+        }
+        public void setIDNumber(int iDNumber) {
+                IDNumber = iDNumber;
+        }
+        public String getChoice() {
+                return choice;
+        }
+        public void setChoice(String choice) {
+                this.choice = choice;
+        }
+}
